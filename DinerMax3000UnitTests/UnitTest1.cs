6b1075d7ebedc19fc91547e2bf661e103070dcb6
CK King <ckking70@gmail.com>
2015-07-03 03:35:53 -0500

    Did Unit Testing About to start Refactoring

---
+++ DinerMax3000UnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DinerMax3000.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var resultMenus = DinerMax3000.Business.Menu.GetAllMenus();
+            Assert.IsTrue(resultMenus.Count > 0);
+
+            int countBeforeSave = resultMenus[0].items.Count;
+            resultMenus[0].SaveMenuItem("UT_Name", "UT_Description", 10.00);
+            resultMenus = DinerMax3000.Business.Menu.GetAllMenus();
+            int countAfterSave = resultMenus[0].items.Count;
+
+            Assert.IsTrue(countBeforeSave < countAfterSave);
+        }
+    }
+}
